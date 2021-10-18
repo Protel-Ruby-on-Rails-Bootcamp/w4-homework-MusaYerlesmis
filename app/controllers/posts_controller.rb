bc5d0bb65aac1d4935efadf501655e710363ccdb
Musa Yerleşmiş <musa.yerlesmis.tr@gmail.com>
2021-10-18 03:26:08 +0300

    firts commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,12 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+
   end
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to posts_path
+      redirect_to dashboard_path
     else
       render :new
     end
