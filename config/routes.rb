5cf2a3139bef2101945e748bbdd4abbcb5031369
Musa Yerleşmiş <musa.yerlesmis.tr@gmail.com>
2021-10-22 23:58:33 +0300

    homework 6 firts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
   get 'dashboard', to: 'dashboard#index', as: 'dashboard'
   
   root 'welcome#index'
+  resources :relationships, only: [:create, :destroy]
 
   devise_for :users
   resources :posts do
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
       patch :accept, on: :member
     end
   end
-      
+  resources :users
+
     
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_17_222335) do
+ActiveRecord::Schema.define(version: 2021_10_22_180731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2021_10_17_222335) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "share"
     t.integer "user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
